fe62467c41308f2c5e8ce07eee70885c20c6e3d6
matrinox <geofflee25@gmail.com>
2017-04-19 13:28:49 -0700

    Add realistic examples for fast ruby ----------------------------------------------------------- On branch master - Wed 19 Apr 2017 13:28:49 PDT by matrinox <geofflee25@gmail.com>

--- code/general/array-argument-vs-splat-arguments.rb
+++ code/general/array-argument-vs-splat-arguments.rb
@@ -2,6 +2,7 @@ require "benchmark/ips"
 
 module M
   ITEMS = (1..10000).to_a.freeze
+  REALISTIC_ITEMS = (1..6).to_a.freeze
 
   def self.func(*args)
   end
@@ -15,8 +16,23 @@ def slow
   M.func(*M::ITEMS)
 end
 
+def realistic_fast
+  M.func(M::REALISTIC_ITEMS)
+end
+
+def realistic_slow
+  M.func(*M::REALISTIC_ITEMS)
+end
+
+def realistic_static_arguments
+  M.func(1, 2, 3, 4, 5, 6)
+end
+
 Benchmark.ips do |x|
   x.report("Function with single Array argument") { fast }
   x.report("Function with splat arguments") { slow }
+  x.report("Function with realistic single Array argument") { realistic_fast }
+  x.report("Function with realistic splat arguments") { realistic_slow }
+  x.report("Function with realistic static arguments") { realistic_static_arguments }
   x.compare!
 end
